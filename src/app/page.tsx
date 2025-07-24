27985fe39f7eed86b166156131caa60e0b7a8589
Ayush Barnwal <ayush.astik@gmail.com>
2025-07-24 23:09:22 +0530

    initial structure commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import LandingPage from '@/components/LandingPage';
+
+export default function HomePage() {
+  return (
+    <main>
+      <LandingPage />
+      {/* Other sections will go here */}
+    </main>
+  );
+}
